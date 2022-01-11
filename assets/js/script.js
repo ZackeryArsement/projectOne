e98b08b6e9d8eadd280e6a81b44ba8e6cc9fb49e
Julie Chu <julieychu@gmail.com>
2022-01-11 13:48:11 -0600

    resizing images and add API keys for google, openweather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // API variables:
-// var googleUrl = 'https://maps.googleapis.com/maps/api/js?key={YOUR_API_KEY}&callback=initMap';
-// var weatherUrl ='api.openweathermap.org/data/2.5/weather?zip=' + userZip ',us&appid={API key}';
-// var 4day-weatherUrl = 'pro.openweathermap.org/data/2.5/forecast/hourly?zip=' + userZip ',us&appid={API key}';
+// var googleUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD2j53y5S7r1DhmM9s62cuB-vC0mPX9TQ8&callback=initMap';
+// var weatherUrl ='api.openweathermap.org/data/2.5/weather?zip=' + userZip ',us&appid=be4771db9c53103bf67e6e18d9ddacc6';
+// var 4day-weatherUrl = 'pro.openweathermap.org/data/2.5/forecast/hourly?zip=' + userZip ',us&appid={be4771db9c53103bf67e6e18d9ddacc6';
 // var instagramUrl = 'https://graph.instagram.com/{media-id}?fields={fields}&access_token={access-token}';
 
-// var userZip = [user input - grab from html input element]
+// var userZip = document.get ElementbyID(""); [user input - grab from html input element]
 
 
 // Starting code for getting APIs:
