2cb65b9a4a58d8f2e32cec29eda1db51996080a6
lane lopez <mrlane51@gmail.com>
2022-01-12 10:20:25 -0600

    added api branch for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,19 +130,48 @@ function clearScreen(){
     }
 }
 
+// function to set the map properties
+function initMap() {
+    var options = {
+        zoom:15,
+        center:{lat:42.3601,lng:-71.0589}
+        }
+        var map = new google.maps.Map(document.getElementById('map'),options);
+    }
+
+document.querySelector('#user-location').click();
+    console.log('clicked')
+
 
 // API variables:
 // var googleUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD2j53y5S7r1DhmM9s62cuB-vC0mPX9TQ8&callback=initMap';
 // var weatherUrl ='api.openweathermap.org/data/2.5/weather?zip=' + userZip ',us&appid=be4771db9c53103bf67e6e18d9ddacc6';
-// var 4day-weatherUrl = 'pro.openweathermap.org/data/2.5/forecast/hourly?zip=' + userZip ',us&appid={be4771db9c53103bf67e6e18d9ddacc6';
-// var instagramUrl = 'https://graph.instagram.com/{media-id}?fields={fields}&access_token={access-token}';
+// var 4day-weatherUrsl = 'pro.openweathermap.org/data/2.5/forecast/hourly?zip=' + userZip ',us&appid=be4771db9c53103bf67e6e18d9ddacc6';
 
 // var userZip = document.get ElementbyID(""); [user input - grab from html input element]
+// **userZip - currently inserted in API variables above - should be replaced by radius search var userChoice**
+// var userChoice = [condition onclick - userHike, userDaytrip, or userGetaway]
+// var userHike = userZip + x miles radius search
+// var userDaytrip = userZip + y miles radius search
+// var userGetaway = userZip + z miles radius search
 
 
-// Starting code for getting APIs:
-// function getApi(googleUrl) {
-//     fetch(googleUrl)
+// Starting code for getting APIs: we will want to parse these by userHike, userDaytrip and userGetaway...
+
+function getApi() {
+    const requestUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD2j53y5S7r1DhmM9s62cuB-vC0mPX9TQ8&callback=initMap';
+    
+    fetch(requestUrl)
+        .then(function (response) {
+        return response.json(); {
+        }
+    })}
+
+//     getApi(googleUrl);
+
+// For Hike and Daytrip
+// function getApi(weatherUrl) {
+//     fetch(weatherUrl)
 //       .then(function (response) {
 //         console.log(response);
 //         if (response.status === 200) {
@@ -152,4 +181,17 @@ function clearScreen(){
 //       });
 //     }
 
-//     getApi(googleUrl);
+//     getApi(weatherUrl);
+
+// For Weekend Getaway
+// function getApi(4day-weatherUrl) {
+//     fetch(googleUrl)
+//       .then(function (response) {
+//         console.log(response);
+//         if (response.status === 200) {
+//             responseText.textContent = response.status;
+//           }
+//           return response.json();
+//       });
+//     }
+//     getApi(4day-weatherUrl);
